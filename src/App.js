ed2651ebf5ee4c6c423fc73f7e7ec4de0f5edce1
Poojana <Poojana.V@ibm.com>
2021-03-04 14:15:26 +0530

    msg change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with this sample project
         </a>
       </header>
     </div>
